8d0aa7b513fb812fef5c4fc11aef15c7911681df
Oleg Sokolovsky <Martiniks.so@gmail.com>
2021-11-05 19:55:20 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToText = path.join(__dirname, 'text.txt');
+
+let  streamFile = fs.createReadStream(pathToText,'utf8');
+
+streamFile.on('data', function (chunk) {
+  // console.log('>>', chunk.length);
+  console.log(chunk);
+});
+
+streamFile.on('error', function (err) {
+  if (err.code === 'ENOENT') {
+    console.log('Файл не найден');
+  } else {
+    console.error(err);
+  }
+});
